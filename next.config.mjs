1c4348d2f9a3247d4bb70f9fd25739d946d2b9d1
Pavan Kumar <pavankumarts2001@gmail.com>
2024-11-25 12:01:29 +0530

    error fetching data

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  images: {
+    domains: ['admin.saaranj.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  },
+}
 
-export default nextConfig;
+export default nextConfig
